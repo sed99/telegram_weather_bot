4047e631c27fb255a6a493ab807f7a35afaa87f4
sed99 <33697908+sed99@users.noreply.github.com>
2018-05-18 22:27:23 +0300

    modified:   weather_bot/weather/api_weather.py
    Added new methods to the Weather class:
    
    	get_currently()
    	get_hourly()
    	get_daily()
    
    They all use the get_data() method, passing as parameters the latitude, longitude, and options (dictionary).
    In the option, the "exclude" key is statically fixed with the required parameters, other keys are freely changeable.
    
    //---------------
    
    Добавлены новые методы в класс Weather:
    
    	get_currently()
    	get_hourly()
    	get_daily()
    
    Все они используют метод  get_data(), передавая в качестве параметров широту, долготу, и опции (словарь).
    В опции статично зафиксирован ключ "exclude" с необходимыми параметрами, остальные ключи свободно изменяемые.

--- weather_bot/weather/api_weather.py
+++ weather_bot/weather/api_weather.py
@@ -19,12 +19,14 @@ class Weather(object):
             raise SecretKeyError(e)
 
         self.key = key  # Your Secret Key
+        self.timeout = 1  # timeout for requests
 
-    def get_data(self, latitude=None, longitude=None, options=None):
+    def get_data(self, latitude, longitude, options):
         """
         Method for requesting data from the site.
         Метод для запроса данных с сайта.
         """
+
         try:
             if not isinstance(latitude, float):
                 e = f"latitude = <class 'float'>. You latitude = {type(latitude)}"
@@ -36,7 +38,7 @@ class Weather(object):
             url_str = "https://api.darksky.net/forecast"
             coord = "%f,%f" % (latitude, longitude)
             key = self.key
-            answer = requests.get("%s/%s/%s" % (url_str, key, coord), params=options, timeout=3)
+            answer = requests.get("%s/%s/%s" % (url_str, key, coord), params=options, timeout=self.time)
             if answer.status_code is 200:
                 return answer.json()
             else:
@@ -44,3 +46,33 @@ class Weather(object):
         except WeatherError as e:
             raise e
 
+    def get_currently(self, latitude, longitude, options=None):  # на данный момент
+        if options is None:
+            options = dict()
+        options["exclude"] = "minutely,hourly,daily,alerts,flags"
+        currently = self.get_data(latitude=latitude, longitude=longitude, options=options)
+        return currently
+
+    def get_hourly(self, latitude, longitude, options=None):
+        """
+        The forecast for the day.
+        Прогноз на 24 часа с времени запроса.
+        """
+
+        if options is None:
+            options = dict()
+        options["exclude"] = "currently,minutely,daily,alerts,flags"
+        hourly = self.get_data(latitude, longitude, options)
+        return hourly
+
+    def get_daily(self, latitude, longitude, options=None):
+        """
+        Weekly forecast.
+        Прогноз на одну неделю.
+        """
+
+        if options is None:
+            options = dict()
+        options["exclude"] = "currently,hourly,minutely,alerts,flags"
+        daily = self.get_data(latitude, longitude, options)
+        return daily
